7212696f6fceba71a4b9ff54d92ca80743260252
u6k <u6k.apps@gmail.com>
2020-04-08 18:28:17 +0900

    Removed: unused import

--- investment_local_horse_racing_trader/health.py
+++ investment_local_horse_racing_trader/health.py
@@ -2,7 +2,6 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
-from time import sleep
 
 options = Options()
 options.add_argument("--disable-extensions")

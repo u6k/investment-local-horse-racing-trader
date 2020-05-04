89ffe0aa41b301e6f9ff86c3cc344a3d8c73942d
u6k <u6k.apps@gmail.com>
2020-05-04 13:55:02 +0900

    Added: critical zone

--- investment_local_horse_racing_trader/flask.py
+++ investment_local_horse_racing_trader/flask.py
@@ -2,6 +2,9 @@ import os
 from flask import Flask, request, g
 import psycopg2
 from psycopg2.extras import DictCursor
+from queue import Queue
+import time
+import functools
 
 from investment_local_horse_racing_trader.app_logging import get_logger
 from investment_local_horse_racing_trader import VERSION, selenium
@@ -13,95 +16,134 @@ logger = get_logger(__name__)
 app = Flask(__name__)
 
 
+singleQueue = Queue(maxsize=1)
+
+
+def multiple_control(q):
+    def _multiple_control(func):
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            q.put(time.time())
+            logger.debug("#multiple_control: start: critical zone")
+            result = func(*args, **kwargs)
+            logger.debug("#multiple_control: end: critical zone")
+            q.get()
+            q.task_done()
+            return result
+        return wrapper
+    return _multiple_control
+
+
 @app.route("/api/health")
 def health():
     logger.info("#health: start")
 
-    result = {"version": VERSION}
-
-    # Check oddspark
-    logger.debug("#health: check oddspark")
-
-    browser = selenium.open_browser()
     try:
+        result = {"version": VERSION}
 
-        selenium.login_oddspark(browser)
+        # Check oddspark
+        logger.debug("#health: check oddspark")
 
-        is_logined_oddspark = selenium.is_logined_oddspark(browser)
-        logger.debug(f"#health: is_logined_oddspark={is_logined_oddspark}")
+        browser = selenium.open_browser()
+        try:
 
-        result["oddspark"] = is_logined_oddspark
+            selenium.login_oddspark(browser)
 
-    finally:
-        browser.close()
-        browser.quit()
+            is_logined_oddspark = selenium.is_logined_oddspark(browser)
+            logger.debug(f"#health: is_logined_oddspark={is_logined_oddspark}")
 
-    # Check database
-    logger.debug("#health: check database")
+            result["oddspark"] = is_logined_oddspark
 
-    db_cursor = get_db().cursor()
-    try:
+        finally:
+            browser.close()
+            browser.quit()
 
-        db_cursor.execute("select 1")
-        result["database"] = True
+        # Check database
+        logger.debug("#health: check database")
 
-    finally:
-        db_cursor.close()
+        db_cursor = get_db().cursor()
+        try:
 
-    return result
+            db_cursor.execute("select 1")
+            result["database"] = True
+
+        finally:
+            db_cursor.close()
+
+        return result
+
+    except Exception:
+        logger.exception("error")
+        return "error", 500
 
 
 @app.route("/api/vote", methods=["POST"])
+@multiple_control(singleQueue)
 def vote():
     logger.info("#vote: start")
 
-    args = request.get_json()
-    logger.debug(f"#vote: args={args}")
+    try:
+        args = request.get_json()
+        logger.debug(f"#vote: args={args}")
 
-    race_id = args.get("race_id", None)
+        race_id = args.get("race_id", None)
 
-    selenium.vote(race_id)
+        selenium.vote(race_id)
 
-    return "ok"
+        return "ok"
+
+    except Exception:
+        logger.exception("error")
+        return "error", 500
 
 
 @app.route("/api/vote_close", methods=["POST"])
 def vote_close():
     logger.info("#vote_close: start")
 
-    args = request.get_json()
-    logger.debug(f"#vote_close: args={args}")
+    try:
+        args = request.get_json()
+        logger.debug(f"#vote_close: args={args}")
 
-    race_id = args.get("race_id", None)
+        race_id = args.get("race_id", None)
 
-    selenium.vote_close(race_id)
+        selenium.vote_close(race_id)
 
-    return "ok"
+        return "ok"
+
+    except Exception:
+        logger.exception("error")
+        return "error", 500
 
 
 @app.route("/api/deposit", methods=["POST"])
 def deposit():
     logger.info("#deposit: start")
 
-    args = request.get_json()
-    logger.debug(f"#deposit: args={args}")
+    try:
+        args = request.get_json()
+        logger.debug(f"#deposit: args={args}")
 
-    deposit_asset = args.get("asset")
+        deposit_asset = args.get("asset")
 
-    predict_result = {
-        "race_id": "deposit",
-        "horse_number": 0,
-        "odds_win": 0,
-        "vote_cost": 0,
-        "parameters": "deposit"
-    }
-    vote_record_id = selenium.store_vote_data(predict_result)
+        predict_result = {
+            "race_id": "deposit",
+            "horse_number": 0,
+            "odds_win": 0,
+            "vote_cost": 0,
+            "parameters": "deposit"
+        }
+        vote_record_id = selenium.store_vote_data(predict_result)
 
-    selenium.store_vote_result(vote_record_id, 0, 0, deposit_asset)
+        selenium.store_vote_result(vote_record_id, 0, 0, deposit_asset)
 
-    last_asset = selenium.get_last_asset()
+        last_asset = selenium.get_last_asset()
 
-    return {"last_asset": last_asset}
+        return {"last_asset": last_asset}
+
+    except Exception:
+        logger.exception("error")
+        return "error", 500
 
 
 def get_db():
